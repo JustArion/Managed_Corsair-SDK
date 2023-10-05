9c756b9048752f14622a4cedaac82998a338c92f
arion <53127594+JustArion@users.noreply.github.com>
2023-10-05 20:19:15 +0200

    Initial LED Implementation

---
+++ CorsairSDK/SDK/CorsairLed.cs
@@ -0,0 +1,141 @@
+﻿namespace Dawn.Libs.Corsair.SDK;
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using LowLevel;
+
+public class CorsairLed
+{
+    internal CorsairLed(ref CorsairDeviceInfo device) => Device = device;
+
+    private readonly CorsairDeviceInfo Device;
+
+    private IDisposable? _deviceControl;
+
+    public unsafe IDisposable RequestControl(CorsairAccessLevel accessLevel)
+    {
+        _deviceControl?.Dispose();
+
+        fixed (sbyte* deviceId = Device.id)
+            Methods.CorsairRequestControl(deviceId, accessLevel).ThrowIfNecessary();
+
+        _deviceControl = LedDisposable.Create(() =>
+        {
+            fixed (sbyte* deviceId = Device.id)
+                Methods.CorsairReleaseControl(deviceId).ThrowIfNecessary();
+        });
+        
+        return _deviceControl;
+    }
+
+    public void ReleaseControl() => _deviceControl?.Dispose();
+    
+    
+    public unsafe (bool Success, CorsairLedPosition[] LedPositions) TryGetLedPositions()
+    {
+        var ledPositions = new CorsairLedPosition[Methods.CORSAIR_DEVICE_LEDCOUNT_MAX];
+        var size = default(int);
+
+        CorsairError error;
+        fixed (CorsairLedPosition* device = &ledPositions[0])
+            fixed (sbyte* deviceId = Device.id)
+            error = Methods.CorsairGetLedPositions(deviceId, (int)Methods.CORSAIR_DEVICE_LEDCOUNT_MAX, device, &size);
+
+        if (error != CorsairError.CE_Success)
+            return (false, Array.Empty<CorsairLedPosition>());
+
+        Array.Resize(ref ledPositions, size);
+
+        return (true, ledPositions);
+    }
+
+
+    public ValueTask<CorsairError> FlushAsync() => SetLedsColorsFlushBufferAsync();
+    public CorsairError Flush(Action<CorsairError> asyncCallback) => SetLedsColorsFlushBuffer(asyncCallback);
+
+    public static async ValueTask<CorsairError> SetLedsColorsFlushBufferAsync()
+    {
+        var id = Guid.NewGuid();
+        var tcs = new TaskCompletionSource<CorsairError>();
+        _asyncCallbacks.Add(id, error => tcs.SetResult(error));
+
+
+        var hGuid = GuidToPointer(id);
+
+        var error = Internal_FlushBufferAsync(hGuid);
+
+        if (error == CorsairError.CE_Success) 
+            return await tcs.Task;
+        
+        _asyncCallbacks.Remove(id);
+        return error;
+
+    }
+
+    private static unsafe nint GuidToPointer(Guid id) => (nint)Unsafe.AsPointer(ref id);
+
+    public static unsafe CorsairError SetLedsColorsFlushBuffer(Action<CorsairError>? onCompletion = null)
+    {
+        if (onCompletion == null)
+            return Methods.CorsairSetLedColorsFlushBufferAsync(&SetLedColorsAsyncCallback, null);
+        
+        var id = Guid.NewGuid();
+        _asyncCallbacks.Add(id, onCompletion);
+
+        var hGuid = GuidToPointer(id);
+
+        return Internal_FlushBufferAsync(hGuid);
+    }
+
+    private static unsafe CorsairError Internal_FlushBufferAsync(nint context) => Methods.CorsairSetLedColorsFlushBufferAsync(&SetLedColorsAsyncCallback, (void*)context);
+
+    // We set the context as a Guid, the guid contains an Action<CorsairError>, this callback allows us to await until the callback completes.
+    // Guid is the indicator for which instance is the caller.
+    // Cleanup involes clearing the Guid KeyValuePair from the Dictionary and freeing the pointer allocated from the instance method.
+    private static readonly Dictionary<Guid, Action<CorsairError>> _asyncCallbacks = new(); 
+    [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvCdecl) })]
+    private static unsafe void SetLedColorsAsyncCallback(void* context, CorsairError error)
+    {
+        if (context == null)
+            return;
+
+        var guid = Unsafe.AsRef<Guid>(context);
+
+        if (!_asyncCallbacks.TryGetValue(guid, out var callback))
+            Console.Error.WriteLine($"[!] {guid} has no matching callback.");
+        else
+        {
+            callback(error);
+            _asyncCallbacks.Remove(guid);
+        }
+    }
+    
+    private class LedDisposable : IDisposable
+    {
+        private LedDisposable(Action cleanupAction) => _CleanupAction = cleanupAction;
+
+        public static IDisposable Create(Action cleanup)
+        {
+            if (cleanup == null) throw new ArgumentNullException(nameof(cleanup));
+            
+            return new LedDisposable(cleanup);
+        }
+
+        private readonly Action _CleanupAction;
+
+        private volatile bool _disposed;
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+            _disposed = true;
+
+            _CleanupAction();
+        }
+    }
+}
+
+public static class CorsairLedEx
+{
+    public static CorsairLed LedControl(this CorsairDeviceInfo info) => new(ref info);
+}
---
+++ CorsairSDK/SDK/Extensions/CorsairLedEx.cs
@@ -0,0 +1,15 @@
+﻿namespace Dawn.Libs.Corsair.SDK.Extensions;
+
+using LowLevel;
+
+public static class CorsairLedEx
+{
+    public static CorsairLedPosition[] GetLedPositions(this CorsairLed led)
+    {
+        var (success, value) = led.TryGetLedPositions();
+        if (!success)
+            throw new Exception(CorsairExtensions.ERROR_PROPERTYINFORMATION);
+
+        return value;
+    }
+}
