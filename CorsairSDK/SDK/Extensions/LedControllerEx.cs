064ddaee6fe55ee29cbc1060e45378e54f3fae22
arion <53127594+JustArion@users.noreply.github.com>
2023-10-06 03:27:09 +0200

    Implementation of the Set Extension methods

--- CorsairSDK/SDK/Extensions/LedControllerEx.cs
+++ CorsairSDK/SDK/Extensions/LedControllerEx.cs
@@ -30,4 +30,45 @@ public static class LedControllerEx
 
         return value;
     }
+    
+    public static void SetLedColor(this LedController ledController, uint id, (byte R, byte G, byte B, byte A) colors) 
+        => SetLedColor(ledController, new CorsairLedColor { id = id, r = colors.R, g = colors.G, b = colors.B, a = colors.A });
+    public static void SetLedColor(this LedController ledController, CorsairLedPosition position, (byte R, byte G, byte B, byte A) colors) 
+        => SetLedColor(ledController, new CorsairLedColor { id = position.id, r = colors.R, g = colors.G, b = colors.B, a = colors.A });
+
+    public static void SetLedColor(this LedController ledController, LedInformation information)
+    {
+        var color = information.Color;
+        if (color.id == default)
+            color = color with { id = information.Position.id };
+
+        SetLedColor(ledController, color);
+    }
+
+    public static void SetLedColor(this LedController ledController, uint id, CorsairLedColor color)
+    {
+        if (color.id == default)
+            color = color with { id = id };
+
+        SetLedColor(ledController, color);
+    }
+
+    public static void SetLedColor(this LedController ledController, CorsairLedPosition position, CorsairLedColor color)
+    {
+        if (color.id == default)
+            color = color with { id = position.id };
+
+        SetLedColor(ledController, color);
+    }
+    
+    public static void SetLedColor(this LedController ledController, CorsairLedColor color)
+    {
+        if (color.id == default)
+            throw new InvalidOperationException("Method requires an LED Id");
+
+        var success = ledController.TrySetLedColor(color);
+
+        if (!success)
+            throw new Exception("Failed to set Led Color");
+    }
 }
