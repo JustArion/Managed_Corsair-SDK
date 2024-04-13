e9b8743a900451182e5a8127f95c0778c19133b1
arion <53127594+JustArion@users.noreply.github.com>
2024-04-13 19:26:53 +0200

    We should maybe do that to the other properties too, since those states can change

--- src/CorsairSDK/Rewrite/Devices/Devices/Headset.cs
+++ src/CorsairSDK/Rewrite/Devices/Devices/Headset.cs
@@ -5,8 +5,23 @@ using System.Diagnostics;
 public class Headset : CorsairDevice
 {
     private int _batteryLevel;
+    private int _lastPropertyUpdate;
+    private bool _surroundSoundEnabled;
+    private bool _sidetoneEnabled;
+    private int _equalizerPreset;
+    private bool _micEnabled;
 
     internal Headset(DeviceInformation deviceInformation) : base(deviceInformation)
+    {
+        UpdateProperties();
+
+        HeadsetOptions = new HeadsetOptions
+        {
+            PropertyUpdateIntervalSeconds = 1
+        };
+    }
+
+    private void UpdateProperties()
     {
         if (SupportedFeatures.Contains(DeviceProperty.SurroundSoundEnabled))
             SurroundSoundEnabled  = _interop.ReadDeviceProperty(Id, DeviceProperty.SurroundSoundEnabled).AsT0;
@@ -24,22 +39,56 @@ public class Headset : CorsairDevice
         if (SupportedFeatures.Contains(DeviceProperty.MicEnabled))
             MicEnabled = _interop.ReadDeviceProperty(Id, DeviceProperty.MicEnabled).AsT0;
 
-        WirelessHeadsetOptions = new HeadsetOptions
-        {
-            BatteryUpdateIntervalSeconds = 1
-        };
+        _lastPropertyUpdate = Environment.TickCount;
     }
 
-    public bool SurroundSoundEnabled { get; }
-    public bool SidetoneEnabled { get; }
-    public int EqualizerPreset { get; }
+    public bool SurroundSoundEnabled
+    {
+        get
+        {
+            if (!PropertiesNeedUpdating())
+                return _surroundSoundEnabled;
 
-    public HeadsetOptions WirelessHeadsetOptions { get; }
+            UpdateProperties();
 
-    private int _lastBatteryUpdate;
+            return _surroundSoundEnabled;
+        }
+        private set => _surroundSoundEnabled = value;
+    }
 
-    private bool BatteryLevelNeedsUpdate() => (Environment.TickCount - _lastBatteryUpdate) * 1000 <
-                                              WirelessHeadsetOptions.BatteryUpdateIntervalSeconds;
+    public bool SidetoneEnabled
+    {
+        get
+        {
+            if (!PropertiesNeedUpdating())
+                return _sidetoneEnabled;
+
+            UpdateProperties();
+
+            return _sidetoneEnabled;
+        }
+        private set => _sidetoneEnabled = value;
+    }
+
+    public int EqualizerPreset
+    {
+        get
+        {
+            if (!PropertiesNeedUpdating())
+                return _equalizerPreset;
+
+            UpdateProperties();
+
+            return _equalizerPreset;
+        }
+        private set => _equalizerPreset = value;
+    }
+
+    public HeadsetOptions HeadsetOptions { get; }
+
+
+    private bool PropertiesNeedUpdating() => (Environment.TickCount - _lastPropertyUpdate) * 1000 <
+                                             HeadsetOptions.PropertyUpdateIntervalSeconds;
     /// <summary>
     /// 100% if the device has no battery
     /// </summary>
@@ -47,18 +96,29 @@ public class Headset : CorsairDevice
     {
         get
         {
-            if (!BatteryLevelNeedsUpdate())
+            if (!PropertiesNeedUpdating())
                 return _batteryLevel;
-            
-            _lastBatteryUpdate = Environment.TickCount;
-            _batteryLevel = _interop.ReadDeviceProperty(Id, DeviceProperty.BatteryLevel).AsT1;
+
+            UpdateProperties();
 
             return _batteryLevel;
         }
         private set => _batteryLevel = value;
     }
 
-    public bool MicEnabled { get; }
+    public bool MicEnabled
+    {
+        get
+        {
+            if (!PropertiesNeedUpdating())
+                return _micEnabled;
+
+            UpdateProperties();
+
+            return _micEnabled;
+        }
+        private set => _micEnabled = value;
+    }
 
     public const int MIN_BATTERY_LEVEL = 0;
     public const int MAX_BATTERY_LEVEL = 100;
@@ -66,5 +126,8 @@ public class Headset : CorsairDevice
 
 public record HeadsetOptions
 {
-    public int BatteryUpdateIntervalSeconds { get; set; }
+    /// <summary>
+    /// If 0, properties won't be cached
+    /// </summary>
+    public uint PropertyUpdateIntervalSeconds { get; set; }
 }
