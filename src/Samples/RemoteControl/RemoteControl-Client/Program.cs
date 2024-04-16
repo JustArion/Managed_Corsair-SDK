eb5433d35f7206ad41b5e8a61d209cbba8139b14
arion <53127594+JustArion@users.noreply.github.com>
2024-04-16 16:40:07 +0200

    Sample Remote Control Client Implemented

---
+++ src/Samples/RemoteControl/RemoteControl-Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using Dawn.Apps.RemoteControlHost;
+using Grpc.Net.Client;
+
+Console.WriteLine("Starting...");
+// const string ADDRESS = "http://localhost:2424";
+const string ADDRESS = "https://split-favorite-our-amongst.trycloudflare.com";
+
+Console.WriteLine($"Connecting to {ADDRESS}");
+using var channel = GrpcChannel.ForAddress(ADDRESS);
+
+var client = new Corsair.CorsairClient(channel);
+
+client.SetGlobal(new Color { A = 255, R = 255, B = 0, G = 0 });
