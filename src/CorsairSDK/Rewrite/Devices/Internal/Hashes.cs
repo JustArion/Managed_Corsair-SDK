c2308db65bd1fb59a95b2edc0964ca32e0980db5
arion <53127594+JustArion@users.noreply.github.com>
2024-05-05 23:43:42 +0200

    Binaries are now embedded and check based on SHA256

---
+++ src/CorsairSDK/Rewrite/Devices/Internal/Hashes.cs
@@ -0,0 +1,8 @@
+﻿namespace Dawn.CorsairSDK.Rewrite.Device.Internal;
+
+internal static class Hashes
+{
+    internal const string X86_SHA256 = "1538ff9fd1e8d96ec9e96a03740f38ec8abca9fd4890f1ff14cdc829082747c8";
+
+    internal const string X64_SHA256 = "94760b5da75ed4cf6bd045ae9b196d758fbb55a39cb3032004fdc321bfd588ee";
+}
--- src/CorsairSDK/Rewrite/Devices/Internal/SDKResolver.cs
+++ src/CorsairSDK/Rewrite/Devices/Internal/SDKResolver.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
+using System.Security.Cryptography;
 
 namespace Dawn.CorsairSDK.Rewrite.Device.Internal;
 
@@ -8,7 +9,11 @@ internal static class SDKResolver
     /// This must be consistent with /ClangSharp/ClangSharpArgs.rsp
     private const string LIBRARY_BINDING_NAME = "iCUESDK";
 
+    private const string X86_FILENAME = "iCUESDK_2019.dll";
+    private const string X64_FILENAME = "iCUESDK.x64_2019.dll";
+
     [SuppressMessage("ReSharper", "SwitchExpressionHandlesSomeKnownEnumValuesWithExceptionInDefault")]
+    // If the binary exists
     internal static nint CorsairSDKResolver(string libraryname, Assembly assembly, DllImportSearchPath? searchpath)
     {
         if (libraryname is not LIBRARY_BINDING_NAME)
@@ -17,21 +22,88 @@ internal static class SDKResolver
         var architecture = RuntimeInformation.OSArchitecture;
 
         var lib = architecture switch {
-            Architecture.X86 => "iCUESDK_2019.dll",
-            Architecture.X64 => "iCUESDK.x64_2019.dll",
-            _ => string.Empty
+            Architecture.X86 => X86_FILENAME,
+            Architecture.X64 => X64_FILENAME,
+            _ => throw new PlatformNotSupportedException()
         };
 
+
+        if (TrySearchForLibrary(lib, architecture, out var ptr))
+            return ptr;
+
+
+        var path = ExtractBinary(lib, Path.Combine(AppContext.BaseDirectory, "Binaries", lib));
+
+        return LoadLibrary(path);
+    }
+
+    private static string ExtractBinary(string resourceKey, string outputFilePath)
+    {
+        var asm = Assembly.GetExecutingAssembly();
+        var resourcePath = asm.GetManifestResourceNames().First(x => x.Contains(resourceKey));
+
+        using var stream = asm.GetManifestResourceStream(resourcePath);
+
+        if (stream is null)
+            throw new FileNotFoundException($"Could not find resource {resourcePath}");
+
+        using var fs = File.Create(outputFilePath);
+        stream.CopyTo(fs);
+
+        return outputFilePath;
+    }
+
+
+    private static bool TrySearchForLibrary(string lib, Architecture architecture, out nint ptr)
+    {
+        ptr = IntPtr.Zero;
 
         var libPath = Path.Combine("Binaries", lib);
 
         var appDirLibPath = Path.Combine(AppContext.BaseDirectory, libPath);
 
-        if (File.Exists(appDirLibPath))
-            return NativeLibrary.Load(appDirLibPath);
 
-        return File.Exists(libPath)
-            ? NativeLibrary.Load(libPath)
-            : IntPtr.Zero;
+        // Corsair's DLLs are signed, but a SHA256 is easier for now.
+
+        if (File.Exists(appDirLibPath))
+        {
+            if (!VerifySHA256(appDirLibPath, architecture))
+                return false;
+
+            ptr = NativeLibrary.Load(appDirLibPath);
+            return true;
+        }
+
+        if (!File.Exists(libPath) || !VerifySHA256(libPath, architecture))
+            return false;
+
+        ptr = NativeLibrary.Load(libPath);
+        return true;
+
+    }
+
+    [SuppressMessage("ReSharper", "SwitchStatementMissingSomeEnumCasesNoDefault")]
+    private static bool VerifySHA256(string path, Architecture architecture) =>
+        architecture switch {
+            Architecture.X86 => IsValidHash(path, Hashes.X86_SHA256),
+            Architecture.X64 => IsValidHash(path, Hashes.X64_SHA256),
+            _ => false
+        };
+
+    private static bool IsValidHash(string path, string expectedHash)
+    {
+
+        byte[] sha256;
+        using (var hFile = File.Open(path, FileMode.Open, FileAccess.Read))
+            sha256 = SHA256.Create().ComputeHash(hFile);
+
+        var actualHash = BitConverter.ToString(sha256).Replace("-", "").ToLower();
+
+        return expectedHash == actualHash;
+    }
+
+    private static nint LoadLibrary(string path)
+    {
+        return NativeLibrary.Load(path);
     }
 }
