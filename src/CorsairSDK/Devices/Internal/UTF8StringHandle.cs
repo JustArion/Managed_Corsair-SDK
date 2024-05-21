f317c95e2387e58631e3ee02c23c29b769d0fd29
arion <53127594+JustArion@users.noreply.github.com>
2024-05-21 03:26:02 +0200

    Hold native memory instead of a GCHandle

--- src/CorsairSDK/Devices/Internal/UTF8StringHandle.cs
+++ src/CorsairSDK/Devices/Internal/UTF8StringHandle.cs
@@ -3,23 +3,41 @@ using System.Text;
 
 namespace Corsair.Device.Internal;
 
-internal unsafe class UTF8StringHandle : IDisposable
+internal sealed unsafe class UTF8StringHandle : IDisposable
 {
-    private GCHandle _handle;
+    private readonly sbyte* _value;
+    private bool _disposed;
 
-    internal UTF8StringHandle(string str)
+    internal UTF8StringHandle(ReadOnlySpan<char> str)
     {
-        var buffer = Encoding.UTF8.GetBytes(str);
-        _handle = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+        var byteLength = Encoding.UTF8.GetByteCount(str);
+
+        _value = (sbyte*)NativeMemory.Alloc((nuint)byteLength);
+        _value[byteLength] = 0; // Ensure Null-Terminated
+
+        Encoding.UTF8.GetBytes(str, new Span<byte>(_value, byteLength));
     }
 
-    public sbyte* Handle => (sbyte*)_handle.AddrOfPinnedObject();
+    public ref readonly sbyte* Value
+    {
+        get
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(UTF8StringHandle));
 
-    public static implicit operator sbyte*(UTF8StringHandle handle) => handle.Handle;
+            return ref _value;
+        }
+    }
+
+    public static implicit operator sbyte*(UTF8StringHandle handle) => handle.Value;
 
     public void Dispose()
     {
-        _handle.Free();
+        if (_disposed)
+            return;
+        _disposed = true;
+
+        NativeMemory.Free(_value);
         GC.SuppressFinalize(this);
     }
 
