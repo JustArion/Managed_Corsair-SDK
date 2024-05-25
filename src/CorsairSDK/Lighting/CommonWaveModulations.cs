6811eaf28aacf49a86e54d22a4c55dfb21810ddc
arion <53127594+JustArion@users.noreply.github.com>
2024-05-25 05:19:57 +0200

    Move to using Common Wave Modulations

---
+++ src/CorsairSDK/Lighting/CommonWaveModulations.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Corsair.Lighting;
+
+public delegate double WaveFunction(float x);
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public static class CommonWaveModulations
+{
+    public static readonly WaveFunction None = f => f;
+
+    public static readonly WaveFunction Sine = f => Math.Sin(f);
+
+    public static readonly WaveFunction Sawtooth = f => f % 1;
+
+    public static readonly WaveFunction Triangle = f => Math.Abs((f % 2) - 1);
+
+    public static readonly WaveFunction Square = f => f % 1 < 0.5 ? 1 : 0;
+
+    // This creates a flicker pattern every 1.5T, the flicker is logaritmic
+    /// <summary>
+    /// sin(  log( tan(x) ) * pi^( sin( x^2 )  )
+    public static readonly WaveFunction Flicker = x =>
+        Math.Sin(
+            Math.Log( Math.Tan(x) ) * Math.Pow(
+                Math.PI, Math.Sin(Math.Pow(x, 2)
+                ))
+        );
+}
+
--- src/CorsairSDK/Lighting/Internal/EffectController.Flicker.cs
+++ src/CorsairSDK/Lighting/Internal/EffectController.Flicker.cs
@@ -4,27 +4,17 @@ namespace Corsair.Lighting.Internal;
 
 internal partial class EffectController
 {
-    // This creates a flicker pattern every 1.5T, the flicker is logaritmic
-    /// <summary>
-    /// sin(  log( tan(x) ) * pi^( sin( x^2 )  )
-    /// </summary>
-    private static readonly WaveFunction _flickerModulation = x =>
-        Math.Sin(
-            Math.Log( Math.Tan(x) ) * Math.Pow(
-                Math.PI, Math.Sin(Math.Pow(x, 2)
-                ))
-        );
     public EffectReceipt FlickerKeys(PulseInfo pulseInfo, params KeyboardKeys[] keys)
-        => PulseKeys(pulseInfo with { WaveModulation = _flickerModulation }, keys);
+        => PulseKeys(pulseInfo with { WaveModulation = CommonWaveModulations.Flicker }, keys);
 
     public EffectReceipt FlickerKeys(PulseInfo pulseInfo, IEnumerable<KeyboardKeys> keys)
-        => PulseKeys(pulseInfo with { WaveModulation = _flickerModulation }, keys);
+        => PulseKeys(pulseInfo with { WaveModulation = CommonWaveModulations.Flicker }, keys);
     public EffectReceipt FlickerKeys(PulseInfo pulseInfo, params KeyboardKey[] keys)
-        => PulseKeys(pulseInfo with { WaveModulation = _flickerModulation }, keys);
+        => PulseKeys(pulseInfo with { WaveModulation = CommonWaveModulations.Flicker }, keys);
 
     public EffectReceipt FlickerKeys(PulseInfo pulseInfo, IEnumerable<KeyboardKey> keys)
-        => PulseKeys(pulseInfo with { WaveModulation = _flickerModulation }, keys);
+        => PulseKeys(pulseInfo with { WaveModulation = CommonWaveModulations.Flicker }, keys);
 
     public EffectReceipt FlickerZones(PulseInfo pulseInfo, KeyboardZones zones)
-        => PulseZones(pulseInfo with { WaveModulation = _flickerModulation }, zones);
+        => PulseZones(pulseInfo with { WaveModulation = CommonWaveModulations.Flicker }, zones);
 }
