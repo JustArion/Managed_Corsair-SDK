5f78cff16cf6dbf11e186a24cfc78e37302d07f3
arion <53127594+JustArion@users.noreply.github.com>
2024-06-17 12:44:26 +0200

    Add underlying system summaries to type

--- src/CorsairSDK/Threading/AtomicBoolean.cs
+++ src/CorsairSDK/Threading/AtomicBoolean.cs
@@ -4,6 +4,11 @@ public struct AtomicBoolean
 {
     public bool GetAndSet(bool newValue) => Convert.ToBoolean(_value.GetAndSet(Convert.ToInt32(newValue)));
 
+    /// <summary>Compares two booleans for equality and, if they are equal, replaces the first value.</summary>
+    /// <param name="value">The value that replaces the destination value if the comparison results in equality.</param>
+    /// <param name="comparand">The value that is compared to the value at <paramref name="location1"/>.</param>
+    /// <returns>The original value in <paramref name="location1"/>.</returns>
+    /// <exception cref="NullReferenceException">The address of <paramref name="location1"/> is a null pointer.</exception>
     public bool CompareAndSet(bool expected, bool newValue) => _value.CompareAndSet(Convert.ToInt32(expected), Convert.ToInt32(newValue));
 
     public bool Value
--- src/CorsairSDK/Threading/AtomicInteger.cs
+++ src/CorsairSDK/Threading/AtomicInteger.cs
@@ -4,6 +4,11 @@ public struct AtomicInteger
 {
     public int GetAndSet(int newValue) => Interlocked.Exchange(ref _value, newValue);
 
+    /// <summary>Compares two 32-bit signed integers for equality and, if they are equal, replaces the first value.</summary>
+    /// <param name="value">The value that replaces the destination value if the comparison results in equality.</param>
+    /// <param name="comparand">The value that is compared to the value at <paramref name="location1"/>.</param>
+    /// <returns>The original value in <paramref name="location1"/>.</returns>
+    /// <exception cref="NullReferenceException">The address of <paramref name="location1"/> is a null pointer.</exception>
     public bool CompareAndSet(int expected, int newValue) => Interlocked.CompareExchange(ref _value, newValue, expected) == expected;
 
     public bool SetIfLessThan(int newValue)
@@ -27,8 +32,16 @@ public struct AtomicInteger
 
     public int GetAndIncrement() => IncrementAndGet() - 1;
 
+    /// <summary>Increments a specified variable and stores the result, as an atomic operation.</summary>
+    /// <param name="location">The variable whose value is to be incremented.</param>
+    /// <returns>The incremented value.</returns>
+    /// <exception cref="NullReferenceException">The address of location is a null pointer.</exception>
     public int IncrementAndGet() => Interlocked.Increment(ref _value);
 
+    /// <summary>Decrements a specified variable and stores the result, as an atomic operation.</summary>
+    /// <param name="location">The variable whose value is to be decremented.</param>
+    /// <returns>The decremented value.</returns>
+    /// <exception cref="NullReferenceException">The address of location is a null pointer.</exception>
     public int DecrementAndGet() => Interlocked.Decrement(ref _value);
 
     private volatile int _value;
