9031a039f2ff62ace5af11502acb76e669f212ad
arion <53127594+JustArion@users.noreply.github.com>
2024-06-21 18:05:25 +0200

    Add Async WaitHandle utils

---
+++ src/CorsairSDK/Threading/Internal/ManualResetEventSlimEx.cs
@@ -0,0 +1,6 @@
+﻿namespace Corsair.Threading.Internal;
+
+internal static class ManualResetEventSlimEx
+{
+    public static Task WaitAsync(this ManualResetEventSlim resetEvent) => WaitHandleAsyncFactory.FromWaitHandle(resetEvent.WaitHandle);
+}
