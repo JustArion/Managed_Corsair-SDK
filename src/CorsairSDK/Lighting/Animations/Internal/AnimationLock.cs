f38bdcb7886bb5977c4d084cb43d30beb4696cc9
arion <53127594+JustArion@users.noreply.github.com>
2024-07-06 15:28:42 +0200

    Device Animation Synchronization

---
+++ src/CorsairSDK/Lighting/Animations/Internal/AnimationLock.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Corsair.Threading;
+
+namespace Corsair.Lighting.Animations.Internal;
+
+internal abstract class AnimationLock : IDisposable
+{
+    private AtomicBoolean _disposed;
+    private readonly TaskCompletionSource _tcs = new();
+
+    public void Release() => Dispose();
+
+    public void Wait() => _tcs.Task.Wait();
+    public Task WaitAsync() => _tcs.Task;
+
+    protected abstract void OnDispose();
+
+    public void Dispose()
+    {
+        if (!_disposed.CompareAndSet(false, true))
+            return;
+
+        OnDispose();
+        _tcs.TrySetResult();
+    }
+}
+
+internal sealed class DisposableAnimationLock(Action onDispose) : AnimationLock
+{
+    protected override void OnDispose() => onDispose();
+}
+
+internal sealed class DisposableAnimationLock<T>(Action<T> onDispose, T state) : AnimationLock
+{
+    protected override void OnDispose() => onDispose(state);
+}
+
---
+++ src/CorsairSDK/Lighting/Animations/Internal/DeviceAnimationSemaphore.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Threading.Async;
+using Corsair.Device;
+using Corsair.Lighting.Internal;
+
+namespace Corsair.Lighting.Animations.Internal;
+
+internal static class DeviceAnimationSemaphore
+{
+    private static readonly Dictionary<string, AnimationLock> _locks = new();
+
+    /// <summary>
+    /// Wait's if the current device is under a lock. Locks the device until released.
+    /// </summary>
+    /// <returns>A lock object that releases the device lock when disposed</returns>
+    public static async Task<AnimationLock> WaitAsync(CorsairDevice device)
+    {
+        if (_locks.TryGetValue(device.Id, out var animationLock))
+            await animationLock.WaitAsync();
+
+        var deviceAnimationLock = new DisposableAnimationLock<CorsairDevice>(OnDispose, device);
+        _locks.Add(device.Id, deviceAnimationLock);
+        return deviceAnimationLock;
+    }
+
+    private static void OnDispose(CorsairDevice dev)
+    {
+        Debug.WriteLine($"Animation lock released for {dev.Type.ToString().ToLower()} device '{dev.Model}'", nameof(DeviceAnimationSemaphore));
+        _locks.Remove(dev.Id);
+    }
+}
