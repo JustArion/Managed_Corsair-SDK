016d0f74a15ba5557dbdd3ff88d4e1337211ee52
arion <53127594+JustArion@users.noreply.github.com>
2024-10-19 02:44:39 +0200

    Log Interop on specific define

--- src/CorsairSDK/Tracing/InteropTracing.cs
+++ src/CorsairSDK/Tracing/InteropTracing.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿// #define DEBUG_INTEROP
+using System.Diagnostics;
 using Corsair.Bindings;
 
 namespace Corsair.Tracing;
@@ -7,26 +8,26 @@ internal static class InteropTracing
 {
     private const string KEY = "Interop";
 
-    [Conditional("DEBUG")]
+    [Conditional("DEBUG_INTEROP")]
     internal static void DebugTrace(CorsairError result, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = default)
         => Debug.WriteLine($"From [{memberName}:{lineNumber}] with result [{result}]", KEY);
 
-    [Conditional("DEBUG")]
+    [Conditional("DEBUG_INTEROP")]
     internal static void DebugTrace<TParam>(CorsairError result, TParam param, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = default)
         => Debug.WriteLine($"From [{memberName}:{lineNumber}] with parameter {param} with result [{result}]", KEY);
 
-    [Conditional("DEBUG")]
+    [Conditional("DEBUG_INTEROP")]
     internal static void DebugTrace<TParam1, TParam2>(CorsairError result, TParam1 param1,
         TParam2 param2, [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = default) =>
         Debug.WriteLine($"From [{memberName}:{lineNumber}] with parameters {param1}, {param2} with result [{result}]", KEY);
 
-    [Conditional("DEBUG")]
+    [Conditional("DEBUG_INTEROP")]
     internal static void DebugTrace<TParam1, TParam2, TParam3>(CorsairError result, TParam1 param1,
         TParam2 param2, TParam3 param3, [CallerMemberName] string memberName = "",
         [CallerLineNumber] int lineNumber = default) =>
         Debug.WriteLine($"From [{memberName}:{lineNumber}] with parameters {param1}, {param2}, {param3} with result [{result}]", KEY);
 
-    [Conditional("DEBUG")]
+    [Conditional("DEBUG_INTEROP")]
     internal static void DebugTrace<TParam1, TParam2, TParam3, TParam4>(CorsairError result,
         TParam1 param1,
         TParam2 param2, TParam3 param3, TParam4 param4, [CallerMemberName] string memberName = "",
