687d52e7f563e257c1b872399bfd22df9bf57ab1
arion <53127594+JustArion@users.noreply.github.com>
2024-10-20 11:02:26 +0200

    Added additional Utility Class

---
+++ src/CorsairSDK/Lighting/KeyUtility.cs
@@ -0,0 +1,6 @@
+﻿namespace Corsair.Lighting;
+
+public static class KeyUtility
+{
+    public static KeyboardKeys[] From(KeyboardKeys start, KeyboardKeys to) => Enum.GetValues<KeyboardKeys>().Where(k => k >= start && k <= to).ToArray();
+}
