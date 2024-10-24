218d6cb35b07adf1c902bf027858484849b73826
arion <53127594+JustArion@users.noreply.github.com>
2024-10-24 02:24:22 +0200

    Added LedGroup (From Corsair)

---
+++ src/CorsairSDK/Lighting/LedGroup.cs
@@ -0,0 +1,51 @@
+﻿using Corsair.Attributes;
+
+namespace Corsair.Lighting;
+
+[DomainImplOf<Bindings.CorsairLedGroup>]
+public enum LedGroup
+{
+    /// <summary>
+    /// For Keyboard Leds
+    /// </summary>
+    Keyboard = 0,
+
+    /// <summary>
+    /// For Keyboard Leds on G-Keys
+    /// </summary>
+    KeyboardGKeys = 1,
+
+    /// <summary>
+    /// For Keyboard Lighting Pipe Leds
+    /// </summary>
+    KeyboardEdge = 2,
+
+    /// <summary>
+    /// For vendor specific Keyboard Leds
+    /// </summary>
+    KeyboardOEM = 3,
+
+    Mouse = 4,
+    Mousemat = 5,
+    Headset = 6,
+    HeadsetStand = 7,
+    MemoryModule = 8,
+    Motherboard = 9,
+    GraphicsCard = 10,
+
+    /// <summary>
+    /// For Leds on the first channel of DIY devices and coolers
+    /// </summary>
+    DIYChannel1 = 11,
+    /// <summary>
+    /// For Leds on the first channel of DIY devices and coolers
+    /// </summary>
+    DIYChannel2 = 12,
+    /// <summary>
+    /// For Leds on the first channel of DIY devices and coolers
+    /// </summary>
+    DIYChannel3 = 13,
+
+    Touchbar = 14,
+    GameController = 15
+}
---
+++ src/CorsairSDK/Lighting/LightingUtilities/GroupUtility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+using Corsair.Device;
+
+namespace Corsair.Lighting.LightingUtilities;
+
+public static class GroupUtility
+{
+    private static ConcurrentDictionary<string, HashSet<KeyboardKey>> _deviceKeys => ZoneUtility._deviceKeys;
+
+    [SuppressMessage("ReSharper", "InvertIf")]
+    public static HashSet<KeyboardKey> GetKeysFromGroup(LedGroup group, CorsairDevice device)
+    {
+        if (!_deviceKeys.TryGetValue(device.Id, out var deviceKeyboardKeys))
+        {
+            var positions = device.LightingInterop.GetPositionInfo();
+
+            deviceKeyboardKeys = [..positions.Select(x => (KeyboardKey)x.Key)];
+            _deviceKeys.TryAdd(device.Id, deviceKeyboardKeys);
+        }
+
+        // The Led Group is the first 16 bits of the int that is the KeyboardKey (id)
+        // See https://corsairofficial.github.io/cue-sdk/#led-identification
+        return deviceKeyboardKeys.Where(x => (int)x >> 16 == (int)group).ToHashSet();
+    }
+}
